3a9045b2210075ff46a754d57c5f571002822d32
mhoang <nguyenhoang150805@gmail.com>
2026-06-15 21:45:20 +0700

    Lab04: upgrade data layer with DI repository EF Core and transaction

--- Labs_1/AspNetWeek1.Api/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-namespace AspNetWeek1.Api.Models
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = String.Empty;
-        public List<Product> Products { get; set; } = new List<Product>();
-    }
-}
